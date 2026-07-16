e22ac2a425045dfeb9842a7b2769bebbb80d114f
Yeshwanti <yeshwanti.pm@gmail.com>
2026-07-16 21:31:52 +0530

    Completed react-hol-6

---
+++ Week 5/trainersapp/src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Cognizant Academy Trainer Portal</h2>
+            <p>Select Trainers to view trainer details.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ Week 5/trainersapp/src/TrainersMock.js
@@ -0,0 +1,30 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+    new Trainer(
+        1,
+        "John",
+        "john@gmail.com",
+        "9876543210",
+        "React",
+        "React, JavaScript, HTML, CSS"
+    ),
+    new Trainer(
+        2,
+        "Priya",
+        "priya@gmail.com",
+        "9876501234",
+        "Angular",
+        "Angular, TypeScript"
+    ),
+    new Trainer(
+        3,
+        "Rahul",
+        "rahul@gmail.com",
+        "9988776655",
+        "Java",
+        "Spring Boot, Hibernate"
+    )
+];
+
+export default trainers;
